edecd87183beda4bb4c6b92ec473953cd9f274bf
e-caste-mbp <en.cas.dev@icloud.com>
2019-03-19 15:12:51 +0100

    deleted 'test' subfolder, added pi_status.py for RaspberryPi status

---
+++ pi_status.py
@@ -0,0 +1,18 @@
+import subprocess
+
+def run_shell_cmd(cmd):
+    # for multiple arguments parse cmd and pass different parts to run
+    # cmd.split(' ')
+    return subprocess.run([cmd], stdout=subprocess.PIPE).stdout.decode('utf-8')
+
+def get_status():
+    uptime_out = run_shell_cmd('uptime')
+    ifconfig_out = subprocess.run(['ifconfig', 'eth0'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    st_out = run_shell_cmd('speedtest')
+    python_out = subprocess.run(['pgrep', '-a', 'python'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    ltl_out = run_shell_cmd('/home/pi/castes-scripts/ltl.sh')
+    return "Uptime:\n" + uptime_out + \
+         "\n\nSpeedtest:\n" + st_out + \
+         "\n\nNetwork status:\n" + ifconfig_out + \
+         "\n\nPython running:\n" + python_out + \
+         "\n\nTwitter bot:\n" + ltl_out
