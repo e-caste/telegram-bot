c2e851c1da2c316490ac4f2aa40208bcc0f59e64
e-caste-mbp <en.cas.dev@icloud.com>
2019-08-29 22:19:40 +0200

    %% don't work. maybe '*.jpg' and not *.jpg will work

--- webcam.py
+++ webcam.py
@@ -36,10 +36,10 @@ def get_yesterday_timelapse_video_name():
                 os.rename(src=webcam_path + yesterday_s + pic, dst=webcam_path + yesterday_s + pic.split(".")[0]
                                                                  + "_" + str(i).zfill(6) + ".jpg")
     # this outputs a 96 fps 30 sec timelapse of yesterday (if pics are taken at 30s intervals = 2880 per day)
-    # os.system("ffmpeg -r 96 -pattern_type glob -i " + webcam_path + yesterday_s + yesterday + "*.jpg -vcodec mpeg4 -y "
-    #           + webcam_path + yesterday_s + yesterday + ".mp4")
-    os.system("ffmpeg -r 96 -i '" + yesterday + "%%%%%%%%%%%%_%6d.jpg' -vcodec mpeg4 -y "
-              + webcam_path + yesterday_s + yesterday + ".mp4" )
+    os.system("ffmpeg -r 96 -pattern_type glob -i '*.jpg' -vcodec mpeg4 -y "
+              + webcam_path + yesterday_s + yesterday + ".mp4")
+    # os.system("ffmpeg -r 96 -i '" + yesterday + "_%%%%%%%%%%%%_%6d.jpg' -vcodec mpeg4 -y "
+    #           + webcam_path + yesterday_s + yesterday + ".mp4" )
     return yesterday
 
 
