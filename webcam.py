11687811df6a564d0d811139ec4a068f5bc13a2c
e-caste-mbp <en.cas.dev@icloud.com>
2019-09-05 11:29:50 +0200

    add .jpg auto-delete after making timelapse

--- webcam.py
+++ webcam.py
@@ -47,10 +47,17 @@ def get_yesterday_timelapse_video_name():
 
     # IMPORTANT: ffmpeg only works with real .jpg and not .png converted into .jpg by changing extension
     # this outputs a 96 fps 30 sec timelapse of yesterday (if pics are taken at 30s intervals = 2880 per day)
-    os.system("ffmpeg -r 96 -f image2 -pattern_type glob -i '" + webcam_path + yesterday_s + "*.jpg' -vcodec mpeg4 -y "
+    ffmpeg_exit_code = os.system("ffmpeg -r 96 -f image2 -pattern_type glob -i '" + webcam_path + yesterday_s + "*.jpg' -vcodec mpeg4 -y "
               + webcam_path + yesterday_s + yesterday + ".mp4")
-    return yesterday
+    # delete all .jpg to save space
+    if ffmpeg_exit_code == 0:
+        for pic in os.listdir(webcam_path + yesterday_s):
+            if pic.endswith(".jpg"):
+                os.remove(webcam_path + yesterday_s + pic)
+    else:
+        print("Error within ffmpeg", file=stderr)
 
+    return yesterday
 
 if __name__ == '__main__':
     # get_last_img_name()
