3793900cb4563240f912fd1b956dc16f9869c597
e-caste-mbp <en.cas.dev@icloud.com>
2019-12-13 13:42:17 +0100

    order stuff

--- telegram_pi_bot.py
+++ telegram_pi_bot.py
@@ -28,10 +28,21 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
+# BEGIN UTILS
 
 def split_msg_for_telegram(string: str):
     chars_per_msg = 4096
     return [string[i:i + chars_per_msg] for i in range(0, len(string), chars_per_msg)]
+
+
+def send_split_msgs(bot, string_list):
+    try:
+        for string in string_list:
+            bot.send_message(chat_id=castes_chat_id, text=string)
+
+    except Exception as e:
+        print("send_split_msgs")
+        print(e)
 
 
 def calculate_time_to_sleep(hour: int, minute: int = 0):
@@ -57,6 +68,10 @@ def calculate_time_to_sleep(hour: int, minute: int = 0):
              - datetime.now()).total_seconds())
     return time_to_sleep
 
+# END UTILS
+
+
+# BEGIN COMMAND HANDLERS
 
 # Define a few command handlers. These usually take the two arguments bot and
 # update. Error handlers also receive the raised TelegramError object in error.
@@ -79,22 +94,31 @@ def chatbot(bot, update):
         bot.send_message(chat_id=update.message.chat_id, text=nmt_result['answers'][nmt_result['best_index']])
 
 
-def secs_per_picture() -> str:
-    """
-    Return the average of seconds per picture taken by Raspberry Pi Zero W
-    """
-    pics = sorted([pic for pic in os.listdir(pics_nas_dir) if pic.endswith(".jpg")])
-    pics_times = [datetime(year=int(pic[0:4]),
-                           month=int(pic[5:7]),
-                           day=int(pic[8:10]),
-                           hour=int(pic[11:13]),
-                           minute=int(pic[13:15]),
-                           second=int(pic[15:17]))
-                  for pic in pics]
-    pics_timedeltas = [(pics_times[i + 1] - pics_times[i]).total_seconds()
-                       for i in range(pics_times[:-1].__len__())]
-    average_time_per_pic = sum(pics_timedeltas) / pics_timedeltas.__len__()
-    return "The average time taken per picture is " + str(round(average_time_per_pic, 2)) + " seconds."
+def epoch(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text=str(int(time())))
+
+
+def whoyouare(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text=str(update.message.from_user))
+
+
+def quote(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text=fortune())
+
+
+def wiki_quote(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text=get_wiki_daily_quote())
+
+
+def error(bot, update):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', bot, update.error)
+
+
+# END COMMAND HANDLERS
+
+
+# BEGIN BUTTON HANDLER w/ FUNCTIONS
 
 
 def button(bot, update):
@@ -262,14 +286,47 @@ def button(bot, update):
         print(e)
 
 
-def send_split_msgs(bot, string_list):
-    try:
-        for string in string_list:
-            bot.send_message(chat_id=castes_chat_id, text=string)
+def get_webcam_img(bot, update):
+    img_name, folder = webcam.get_last_img_name()
+    if folder:
+        path_name = folder + "/" + img_name
+    else:
+        path_name = img_name
+    bot.send_photo(chat_id=update.callback_query.message.chat_id,
+                   photo=open(webcam_path + path_name, 'rb'),
+                   caption=img_name)
 
-    except Exception as e:
-        print("send_split_msgs")
-        print(e)
+
+def get_webcam_timelapse(bot, update):
+    yesterday = webcam.get_yesterday_timelapse_video_name()
+    bot.send_video(chat_id=update.callback_query.message.chat_id,
+                   video=open(webcam_path + yesterday + "/" + yesterday + "_for_tg.mp4", 'rb'),
+                   caption=yesterday,
+                   timeout=6000)
+
+
+def secs_per_picture() -> str:
+    """
+    Return the average of seconds per picture taken by Raspberry Pi Zero W
+    """
+    pics = sorted([pic for pic in os.listdir(pics_nas_dir) if pic.endswith(".jpg")])
+    pics_times = [datetime(year=int(pic[0:4]),
+                           month=int(pic[5:7]),
+                           day=int(pic[8:10]),
+                           hour=int(pic[11:13]),
+                           minute=int(pic[13:15]),
+                           second=int(pic[15:17]))
+                  for pic in pics]
+    pics_timedeltas = [(pics_times[i + 1] - pics_times[i]).total_seconds()
+                       for i in range(pics_times[:-1].__len__())]
+    average_time_per_pic = sum(pics_timedeltas) / pics_timedeltas.__len__()
+    return "The average time taken per picture is " + str(round(average_time_per_pic, 2)) + " seconds."
+
+
+# END BUTTON HANDLER w/ FUNCTIONS
+
+
+# BEGIN PERIODIC JOBS
 
 
 def check_for_new_events(bot, hour: int):
@@ -315,25 +372,6 @@ def check_for_new_events(bot, hour: int):
             print(e, file=sys.stderr)
 
 
-def get_webcam_img(bot, update):
-    img_name, folder = webcam.get_last_img_name()
-    if folder:
-        path_name = folder + "/" + img_name
-    else:
-        path_name = img_name
-    bot.send_photo(chat_id=update.callback_query.message.chat_id,
-                   photo=open(webcam_path + path_name, 'rb'),
-                   caption=img_name)
-
-
-def get_webcam_timelapse(bot, update):
-    yesterday = webcam.get_yesterday_timelapse_video_name()
-    bot.send_video(chat_id=update.callback_query.message.chat_id,
-                   video=open(webcam_path + yesterday + "/" + yesterday + "_for_tg.mp4", 'rb'),
-                   caption=yesterday,
-                   timeout=6000)
-
-
 def make_new_webcam_timelapse(hour: int, minute: int):
     while True:
         try:
@@ -374,25 +412,7 @@ def send_timelapse_notification(bot, hour: int, minute: int):
             print(e, file=sys.stderr)
 
 
-def epoch(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text=str(int(time())))
-
-
-def whoyouare(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text=str(update.message.from_user))
-
-
-def quote(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text=fortune())
-
-
-def wiki_quote(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text=get_wiki_daily_quote())
-
-
-def error(bot, update):
-    """Log Errors caused by Updates."""
-    logger.warning('Update "%s" caused error "%s"', bot, update.error)
+# END PERIODIC JOBS
 
 
 def main():
