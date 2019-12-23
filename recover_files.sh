826e094ee3b5c1960a5c96546488759a3aaf4d32
e-caste-mbp <en.cas.dev@icloud.com>
2019-12-23 16:31:10 +0100

    add script content

---
+++ recover_files.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+DAYS=()
+TODAY=$(date +%Y-%m-%d)
+
+# add only days before today to array
+for item in *.jpg ; do
+    DAY="${item:0:10}"
+    if [ $DAY != $TODAY ]; then
+        if [[ ! " ${DAYS[*]} " == *"$DAY"* ]]; then
+            DAYS+=("$DAY")
+        fi
+    fi
+done
+
+# for debugging
+# echo "${DAYS[@]}"
+# exit 0
+
+if [ ${#DAYS[@]} -eq 0 ]; then
+    echo "There are no past days to make the timelapse for."
+    exit 42
+fi
+
+# make 2 timelapses (high quality and for telegram) for each of past days
+for DAY in "${DAYS[@]}"; do
+    mkdir $DAY
+    echo "Made directory $DAY"
+    mv "$DAY"*.jpg $DAY
+    echo "Moved all $DAY*.jpgs to $DAY directory, now running ffmpeg..."
+    cd $DAY
+    ffmpeg -r 96 -f image2 -pattern_type glob -i '*.jpg' -c:v libx264 -b:v 31457280 -y "$DAY"_full_quality.mp4 && ffmpeg -i "$DAY"_full_quality.mp4 -r 60 -b:v 2500000 -c:v libx264 -profile:v high -pix_fmt yuv420p "$DAY"_for_tg.mp4
+    echo "ffmpeg has run, now removing .jpgs..."
+    rm *.jpg
+    echo "Removed all .jpgs for $DAY"
+    cd ..
+done
+
+exit 0
