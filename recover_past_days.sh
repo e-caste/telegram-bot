26f6b3150ba3fca56345194588c02782fb88b200
e-caste-mbp <en.cas.dev@icloud.com>
2019-12-27 11:09:27 +0100

    fix logic

--- recover_past_days.sh
+++ recover_past_days.sh
@@ -19,22 +19,21 @@ done
 
 if [ ${#DAYS[@]} -eq 0 ]; then
     echo "Missing both videos: There are no past days to make the timelapse for."
-    exit 42
+else
+    # make 2 timelapses (high quality and for telegram) for each of past days
+    for DAY in "${DAYS[@]}"; do
+        mkdir "$DAY"
+        echo "Made directory $DAY"
+        mv "$DAY"*.jpg $DAY
+        echo "Moved all $DAY*.jpgs to $DAY directory, now running ffmpeg..."
+        cd $DAY
+        ffmpeg -r 96 -f image2 -pattern_type glob -i '*.jpg' -c:v libx264 -b:v 31457280 -y "$DAY"_full_quality.mp4 && ffmpeg -i "$DAY"_full_quality.mp4 -r 60 -b:v 2500000 -c:v libx264 -profile:v high -pix_fmt yuv420p "$DAY"_for_tg.mp4
+        echo "ffmpeg has run, now removing .jpgs..."
+        rm *.jpg
+        echo "Removed all .jpgs for $DAY"
+        cd ..
+    done
 fi
-
-# make 2 timelapses (high quality and for telegram) for each of past days
-for DAY in "${DAYS[@]}"; do
-    mkdir "$DAY"
-    echo "Made directory $DAY"
-    mv "$DAY"*.jpg $DAY
-    echo "Moved all $DAY*.jpgs to $DAY directory, now running ffmpeg..."
-    cd $DAY
-    ffmpeg -r 96 -f image2 -pattern_type glob -i '*.jpg' -c:v libx264 -b:v 31457280 -y "$DAY"_full_quality.mp4 && ffmpeg -i "$DAY"_full_quality.mp4 -r 60 -b:v 2500000 -c:v libx264 -profile:v high -pix_fmt yuv420p "$DAY"_for_tg.mp4
-    echo "ffmpeg has run, now removing .jpgs..."
-    rm *.jpg
-    echo "Removed all .jpgs for $DAY"
-    cd ..
-done
 
 
 DAYS=()
